1522b3e70d34903b99d131344b5cc6c879327443
daddybunz <pon3kub@gmail.com>
2020-08-07 15:18:21 +0700

    Edit route And Add /company/ready Page

--- src/components/company/manage/RewardTicket.js
+++ src/components/company/manage/RewardTicket.js
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
-import { CDataTable, CCardBody, CCard, CCardHeader } from "@coreui/react";
+import {
+  CDataTable,
+  CCardBody,
+  CCard,
+  CCardHeader,
+  CRow,
+  CInput,
+  CForm,
+  CCol,
+  CButton,
+} from "@coreui/react";
 
 const fields = [
-  { key: "name", _style: { width: "20%" } },
-  { key: "schemaName", _style: { width: "25%" } },
-  { key: "createdAt", _style: { width: "25%" } },
-  { key: "manage", _style: { width: "15%" }, sorter: false, filter: false },
-  { key: "actions", _style: { width: "5%" }, sorter: false, filter: false },
+  { key: "ticketId", _style: { width: "15%" } },
+  { key: "steamId", label: "Steam ID & Comment ID", _style: { width: "20%" } },
+  { key: "name", _style: { width: "15%" } },
+  { key: "createdAt", _style: { width: "15%" } },
+  { key: "createTime", _style: { width: "15%" } },
+  { key: "URL", _style: { width: "15%" } },
 ];
 
 const usersData = [
   {
-    id: 0,
+    ticketId: 0,
+    steamId: "dasdasd",
     name: "John Doe",
-    schemaName: "Guest",
     createdAt: "2018/01/01",
+    createTime: "Guest",
+    URL: "www",
   },
 ];
 
@@ -22,36 +35,73 @@ function RewardTicket() {
   //const [details, setDetails] = useState([]);
   const [items] = useState(usersData);
   return (
-    <CCard>
-    <CCardHeader>
-        <p style={{ paddingLeft: 15, marginTop: "auto" }}>
-          Ticket ({items.length})
-        </p>
-    </CCardHeader>
-    <CCardBody>
-      <br />
-      <CDataTable
-        items={usersData}
-        fields={fields}
-        striped
-        responsive
-        columnFilter
-        tableFilter
-        footer
-        itemsPerPageSelect
-        itemsPerPage={10}
-        outlined
-        hover
-        sorter
-        pagination
-        addTableClasses={{}}
-        scopedSlots={{
-          
-        }}
-      />
-    </CCardBody>
-  </CCard>
+    <div>
+      <CCard>
+        <CCardBody>
+          <CForm action="" method="post">
+            <CRow>
+              <CCol sm="2">
+                <p style={{ margin: "auto" }}>Define Period</p>
+              </CCol>
+              <CCol sm="4">
+                <CInput type="date" />
+              </CCol>
+              <CCol sm="4">
+                <CInput type="date" />
+              </CCol>
+              <CCol sm="2">
+                <CButton style={{ width: "100%" }} color={"info"}>
+                  Search
+                </CButton>
+              </CCol>
+            </CRow>
+          </CForm>
+        </CCardBody>
+      </CCard>
 
+      <CCard>
+        <CCardHeader>
+          <p style={{ paddingLeft: 15, marginTop: "auto" }}>
+            Ticket ({items.length})
+          </p>
+        </CCardHeader>
+        <CCardBody>
+          <br />
+          <CDataTable
+            items={usersData}
+            fields={fields}
+            striped
+            responsive
+            columnFilter
+            tableFilter
+            footer
+            itemsPerPageSelect
+            itemsPerPage={10}
+            outlined
+            hover
+            sorter
+            pagination
+            addTableClasses={{}}
+            scopedSlots={{}}
+          />
+        </CCardBody>
+      </CCard>
+
+      <CCard>
+        <CCardHeader>Reward Ticket</CCardHeader>
+        <CCardBody>
+          <CForm action="" method="post">
+            <CRow>
+              <CCol sm="12">
+                <p>Ticket ID</p>
+                <CInput type="text" placeholder="Ticket ID" />
+              </CCol>
+            </CRow><br/>
+            <CButton color="info" >SUBMIT</CButton>
+          </CForm>
+        </CCardBody>
+      </CCard>
+    </div>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -3,34 +3,34 @@ import React from 'react';
 
 const Dashboard = React.lazy(() => import('./components/Dashboard'));
 // Company
-const Company = React.lazy(()=> import('./components/company/index'))
-const RewardTicket = React.lazy(()=> import('./components/company/manage/RewardTicket'))
-const Ready = React.lazy(()=> import('./components/company/manage/Ready'))
-const Mention = React.lazy(()=> import('./components/company/manage/Mention'))
-const TokenPantip = React.lazy(()=> import('./components/company/manage/TokenPantip'))
-const JobQueue = React.lazy(()=> import('./components/company/manage/JobQueue'))
-const TriggerCondition = React.lazy(()=> import('./components/company/manage/TriggerCondition'))
-const Bida = React.lazy(()=> import('./components/company/manage/Bida'))
-const FeedHistory = React.lazy(()=> import('./components/company/manage/FeedHistory'))
-const ManualAddLink = React.lazy(()=> import('./components/company/manage/ManualAddLink'))
+const Company = React.lazy(()=> import('./components/company/index'));
+const RewardTicket = React.lazy(()=> import('./components/company/manage/RewardTicket'));
+const Ready = React.lazy(()=> import('./components/company/manage/Ready'));
+const Mention = React.lazy(()=> import('./components/company/manage/Mention'));
+const TokenPantip = React.lazy(()=> import('./components/company/manage/TokenPantip'));
+const JobQueue = React.lazy(()=> import('./components/company/manage/JobQueue'));
+const TriggerCondition = React.lazy(()=> import('./components/company/manage/TriggerCondition'));
+const Bida = React.lazy(()=> import('./components/company/manage/Bida'));
+const FeedHistory = React.lazy(()=> import('./components/company/manage/FeedHistory'));
+const ManualAddLink = React.lazy(()=> import('./components/company/manage/ManualAddLink'));
 //User
-const ManageUser = React.lazy(() => import('./components/user/ManageUser'))
-const UserOnline = React.lazy(() => import('./components/user/UserOnline'))
+const ManageUser = React.lazy(() => import('./components/user/ManageUser'));
+const UserOnline = React.lazy(() => import('./components/user/UserOnline'));
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
 
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-  { path: '/company', name: 'Company', component: Company },
-  { path: '/company/rewardticket', name: 'Reward Ticket', component: RewardTicket },
-  { path: '/company/ready', name: 'Ready', component: Ready },
-  { path: '/company/mention', name: 'Mention', component: Mention },
-  { path: '/company/tokenpantip', name: 'Token Pantip', component: TokenPantip },
-  { path: '/company/jobqueue', name: 'Job Queue', component: JobQueue },
-  { path: '/company/triggercondition', name: 'Trigger Condition', component: TriggerCondition },
-  { path: '/company/bida', name: 'Bida', component: Bida },
-  { path: '/company/feedhistory', name: 'Feed History', component: FeedHistory },
-  { path: '/company/manualaddlink', name: 'Manual Add Link', component: ManualAddLink },
+  { path: '/company', exact: true, name: 'Company', component: Company },
+  { path: '/company/rewardticket', exact: true, name: 'Reward Ticket', component: RewardTicket },
+  { path: '/company/ready', exact: true, name: 'Ready', component: Ready },
+  { path: '/company/mention', exact: true, name: 'Mention', component: Mention },
+  { path: '/company/tokenpantip', exact: true, name: 'Token Pantip', component: TokenPantip },
+  { path: '/company/jobqueue', exact: true, name: 'Job Queue', component: JobQueue },
+  { path: '/company/triggercondition', exact: true, name: 'Trigger Condition', component: TriggerCondition },
+  { path: '/company/bida', exact: true, name: 'Bida', component: Bida },
+  { path: '/company/feedhistory', exact: true, name: 'Feed History', component: FeedHistory },
+  { path: '/company/manualaddlink', exact: true, name: 'Manual Add Link', component: ManualAddLink },
 
   { path: '/user/manage-user', name: 'Manage User', component: ManageUser },
   { path: '/user/user-online', name: 'User Online', component: UserOnline },
